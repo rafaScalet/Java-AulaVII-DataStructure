ae05c6d790c9cdec914c579f4adc99a77091981a
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-21 10:22:39 -0300

    completed

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,17 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.printf("Byte MinValue: %20d\n", Byte.MIN_VALUE);
+        System.out.printf("Byte MaxValue: %20d\n", Byte.MAX_VALUE);
+        System.out.printf("Short MinValue: %20d\n", Short.MIN_VALUE);
+        System.out.printf("Short MaxValue: %20d\n", Short.MAX_VALUE);
+        System.out.printf("Int MinValue: %20d\n", Integer.MIN_VALUE);
+        System.out.printf("Int MaxValue: %20d\n", Integer.MAX_VALUE);
+        System.out.printf("Long MinValue: %20d\n", Long.MIN_VALUE);
+        System.out.printf("Long MaxValue: %20d\n", Long.MAX_VALUE);
+        System.out.printf("Float MinValue: %10f\n", Float.MIN_VALUE);
+        System.out.printf("Float MaxValue: %10f\n", Float.MAX_VALUE);
+        System.out.printf("Double MinValue: %10f\n", Double.MIN_VALUE);
+        System.out.printf("Double MaxValue: %10f\n", Double.MAX_VALUE);
     }
 }
